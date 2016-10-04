303720167b801fa4bef5ddbb9cf2fc95f1b60084
ledomone <ledom.git@gmail.com>
2016-10-04 23:24:37 +0200

    Routing

--- src/app.js
+++ src/app.js
@@ -4,7 +4,23 @@ import ReactDOM from 'react-dom'
 import 'font-awesome/css/font-awesome.css'
 import './app.css'
 
+import { browserHistory, Router, Route } from 'react-router'
+
+class Home extends React.Component {
+	render() {
+		return (<div>Hello world</div>)
+	}
+}
+
+const routes = (
+	<Router>
+		<Route path="/" component={Home} />
+	</Router>
+)
+
+
 import App from 'containers/App/App'
 
 const mountNode = document.querySelector('#root');
-ReactDOM.render(<App />, mountNode);
+ReactDOM.render(
+	<App history={browserHistory} routes={routes} />, mountNode);

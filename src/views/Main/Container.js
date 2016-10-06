a73bc6eeb42fe19e705fb5a440621395bc55eb58
Dominik <ledom.git@gmail.com>
2016-10-06 15:55:01 +0200

    Getting a List of Places

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,13 +1,48 @@
 import React from 'react'
 import Map, {GoogleApiWrapper} from 'google-maps-react'
+import {searchNearby} from 'utils/googleApiHelpers'
 
 export class Container extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			places: [],
+			pagination: null
+		}
+	}
+	onReady(mapProps, map) {
+		const {google} = this.props;
+		const opts = {
+			location: map.center,
+			radius: '500',
+			types: ['cafe']
+		}
+		searchNearby(google, map, opts)
+			.then((results, pagination) => {
+				// We got some results and a pagination object
+				this.setState({
+					places: results,
+					pagination
+				});
+			}).catch((status, result) => {
+				// There was an error
+			});
+	}
 	render() {
 		return (
 			<div>
 				Hello from the container (other side ;-) )
 				<Map
-					google={this.props.google} />
+					onReady={this.onReady.bind(this)}
+					google={this.props.google}
+					visible={false}>
+
+					{this.state.places.map(place => {
+						return (<div key={place.id}>{place.name}</div>)
+					})}
+
+				</Map>
 			</div>
 		)
 	}

14fb46b67b03d908ae6c920e1a4d8115cafc2ec8
ledomone <ledom.git@gmail.com>
2016-10-12 17:15:04 +0200

    Clicking on Markers

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -36,6 +36,13 @@ export class Container extends React.Component {
 				// There was an error
 			});
 	}
+
+	onMarkerClick(item) {
+		const {place} = item; // place prop
+		const {push} = this.context.router;
+		push(`/map/detail/${place.place_id}`)
+	}
+
 	render() {
 		let children = null;
 		if (this.props.children) {
@@ -45,7 +52,8 @@ export class Container extends React.Component {
 				{
 					google: this.props.google,
 					places: this.state.places,
-					loaded: this.props.loaded
+					loaded: this.props.loaded,
+					onMarkerClick: this.onMarkerClick.bind(this)
 				}
 			);
 		}
@@ -70,6 +78,10 @@ export class Container extends React.Component {
 			</Map>
 		)
 	}
+}
+
+Container.contextTypes = {
+	router: React.PropTypes.object
 }
 
 export default GoogleApiWrapper({

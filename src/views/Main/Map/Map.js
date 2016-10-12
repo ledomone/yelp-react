3dea89d9fa3bdd7fc4c350fb2901927c6841aba7
ledomone <ledom.git@gmail.com>
2016-10-12 16:58:14 +0200

    Creating Markers On Our Map

--- src/views/Main/Map/Map.js
+++ src/views/Main/Map/Map.js
@@ -1,15 +1,27 @@
 import React, { PropTypes as T } from 'react'
 import classnames from 'classnames'
-import Map from 'google-maps-react'
+import Map, { Marker } from 'google-maps-react'
 
 import styles from './styles.module.css'
 
 export class MapComponent extends React.Component {
+	renderMarkers() {
+		if (!this.props.places) { return null; }
+		return this.props.places.map(place => {
+			return (
+				<Marker key={place.id}
+					name={place.id}
+					place={place}
+					position={place.geometry.location}
+				/>
+			)
+		})
+	}
 	render() {
 		return (
 			<Map google={this.props.google}
 				className={styles.map}>
-				
+				{this.renderMarkers()}
 			</Map>
 		)
 	}

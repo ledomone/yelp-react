7e76cd58eac495d47e2fc0aaa8cd06c507eaf8cb
ledomone <ledom.git@gmail.com>
2016-10-13 16:46:53 +0200

    Adding More Details to Markers - Part I

--- src/views/Main/routes.js
+++ src/views/Main/routes.js
@@ -2,12 +2,16 @@ import React from 'react'
 import {Route} from 'react-router'
 import Container from './Container'
 import Map from './Map/Map'
+import Detail from './Detail/Detail'
 
 export const makeMainRoutes = () => {
 
 	return (
 		<Route path="/" component={Container}>
-			<Route path="map" component={Map} />
+			<Route path="map" component={Map}>
+				<Route path="detail/:placeId"
+					component={Detail} />
+			</Route>
 		</Route>
 	)
 }
